aa1345f2d79b966c70aab81a3a0fe819221fbe76
kawashita kenji <leonardkenji@gmail.com>
2026-04-07 00:35:40 +0900

    started the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,24 @@
+import { __unstable__loadDesignSystem } from "tailwindcss";
+
 export const Hero = () => {
-  return <section></section>
+  return <section className="relative min-h-screen flex items-center overflow-hidden">
+
+    {/* black dots */}
+    <div className= "absolute inset-0 overflow-hidden pointer-events-none">
+    {[...Array(30)].map((_, i) => (
+      <div className="absolute w-1 h-1 rounded-full opacity-20"
+      style={{
+        backgroundColor: "#000000",
+        left: `${Math.random() * 100}%`,
+        top: `${Math.random() * 100}%`,
+        animation: `slow-drift 30s ease-in-out infinite`
+      }}
+    />
+    ))}
+    </div>
+{/* Content */}
+    <div>
+      <h1>Leonardo Kenji Kawashita</h1>
+    </div>
+  </section>
 };
