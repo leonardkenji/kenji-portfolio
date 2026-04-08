6f03ffd02da4f25598baef45f44b16368d5eb276
kawashita kenji <leonardkenji@gmail.com>
2026-04-08 20:46:28 +0900

    up to the Hero section image

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,7 +6,7 @@ const navLinks = [{href:"#about", label:"About"}, {href:"#experience", label:"Ex
 
 export const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  return <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+  return <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
     <nav className="container mx-auto px-6 flex items-center justify-between">
       <a href="#" className="text-xl font-bold hover:text-primary">
         LK<span className="text-primary">.</span>
