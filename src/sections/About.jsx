1b426d2c8ad2e64e9f546921a77ea17ef42e1ca2
kawashita kenji <leonardkenji@gmail.com>
2026-04-12 21:29:08 +0900

    made the text about me justify-center

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return <section className="md:hidden px-4 py-4">
           <div className="mx-auto max-w-4xl rounded-3xl bg-primary-foreground px-6 py-5">
-            <div className="md:hidden animate-fade-in leading-loose text-justify">
+            <div className="md:hidden animate-fade-in leading-loose text-center">
               <span className="text-primary font-bold text-2xl">Full-Stack Software Engineer</span> based in Japan, specializing in web applications with <span className="text-primary font-bold">AI-driven features</span>. I combine strong technical skills with a solid background in digital marketing to build products that are both scalable and user-focused. Previously worked in end-to-end marketing roles at venture and global companies, including <span className="font-bold">Easy car RIDE</span>, <span className="font-bold">Tokyo Supercars</span>, <span className="font-bold">Croud (London, remote)</span>, and <span className="font-bold">Edelman PR (São Paulo)</span>. This experience allows me to <span className="text-primary font-bold">bridge business strategy and technical execution</span>. Currently focused on developing AI-powered applications and open to opportunities in product, growth, or full-stack engineering roles.
             </div>
           </div>
